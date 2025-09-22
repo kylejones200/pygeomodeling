283be55d7232488a85d01f60cdb40020b1eaba27
kylejones200 <kyletjones@gmail.com>
2025-09-22 16:55:22 -0500

    chore: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pygeomodeling",
-    version="0.1.0",
+    version="0.1.1",
     description="Gaussian Process Regression and Kriging for 3D Reservoir Simulation Data",
     author="Kyle T. Jones",
     author_email="kyletjones@gmail.com",
